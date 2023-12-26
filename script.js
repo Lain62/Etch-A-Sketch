ac12387fcad87fbc64a4ac878931b5f252708555
Lain <145809470+Lain62@users.noreply.github.com>
2023-12-26 23:37:59 +0700

    added 16x16 boxes with change on click

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello world")
+const divContainerBoard = document.querySelector('.containerBoard');
+
+function boxesCreate(){
+	for (let x = 1; x <= 16; x++){
+		const collumnBox = document.createElement('div');
+		collumnBox.classList.add('collumnBox');
+		divContainerBoard.appendChild(collumnBox)
+		for (let y = 1; y <= 16; y++){
+			const box = document.createElement('div');
+			box.classList.add('box');
+			collumnBox.appendChild(box);
+			box.addEventListener('click', function (e) {
+				e.target.style.background = 'black';
+			});
+		}
+	}
+}
+
+boxesCreate();
